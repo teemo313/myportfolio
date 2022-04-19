183cd68e035562d0ad478934ad1ab3fd3d730110
teemo313 <dbsehd2309@naver.com>
2022-04-20 00:04:22 +0900

    [Edit] edit notion link

--- src/Footer.js
+++ src/Footer.js
@@ -10,7 +10,7 @@ const Footer = () =>{
             <div>
                 <h1 className='footh1'>Contact</h1>
                 <p className='footp' style={{marginTop:'0'}}>E-mail : dbsehd2309@naver.com</p>
-                <p className='footp' style={{marginTop:'0'}}>Notion : ddddd</p>
+                <p className='footp' style={{marginTop:'0'}}>Notion : notion</p>
                 <p className='footp' style={{marginTop:'0'}}>Github : ddddddddd</p>
             </div>
         </div>
